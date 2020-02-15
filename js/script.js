cf6a8d17efd3d451937f7a1edd537e7b0dbef4ee
Filip Bendevski <bende@localhost.localdomain>
2020-02-15 13:01:04 +0400

    I'm done for now

--- js/script.js
+++ js/script.js
@@ -12,7 +12,7 @@ let stopColors;
 function randBetween(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min; 
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 
@@ -28,21 +28,21 @@ function waw() {
     audio = new Audio("Sound/ohNo.mp3");
 }
 
-function theyDidIt(){
-    function movement(direction){
-        switch(direction){
-            case(RIGHT):
-                if (randBetween(0,1)===0) return [-randBetween(8,20), 20, randBetween(0,1)? -1:1, 0];
-                else return [randBetween(8,20), 20, randBetween(0,1)? -1:1, 0];
-            case(UP):
-                if (randBetween(0,1)===0) return [20, -randBetween(8,20), 0, randBetween(0,1)? -1:1];
-                else return [20, randBetween(8,20), 0, randBetween(0,1)? -1:1];
-            case(LEFT):
-                if (randBetween(0,1)===0) return [-randBetween(8,20), -20, randBetween(0,1)? -1:1, 0];
-                else return [randBetween(8,20), -20, randBetween(0,1)? -1:1, 0];
-            case(DOWN):
-                if (randBetween(0,1)===0) return [-20, -randBetween(8,20), 0, randBetween(0,1)? -1:1];
-                else return [-20, randBetween(8,20), 0, randBetween(0,1)? -1:1];
+function theyDidIt() {
+    function movement(direction) {
+        switch (direction) {
+            case (RIGHT):
+                if (randBetween(0, 1) === 0) return [-randBetween(8, 20), 20, randBetween(0, 1) ? -1 : 1, 0];
+                else return [randBetween(8, 20), 20, randBetween(0, 1) ? -1 : 1, 0];
+            case (UP):
+                if (randBetween(0, 1) === 0) return [20, -randBetween(8, 20), 0, randBetween(0, 1) ? -1 : 1];
+                else return [20, randBetween(8, 20), 0, randBetween(0, 1) ? -1 : 1];
+            case (LEFT):
+                if (randBetween(0, 1) === 0) return [-randBetween(8, 20), -20, randBetween(0, 1) ? -1 : 1, 0];
+                else return [randBetween(8, 20), -20, randBetween(0, 1) ? -1 : 1, 0];
+            case (DOWN):
+                if (randBetween(0, 1) === 0) return [-20, -randBetween(8, 20), 0, randBetween(0, 1) ? -1 : 1];
+                else return [-20, randBetween(8, 20), 0, randBetween(0, 1) ? -1 : 1];
         }
     }
 
@@ -56,88 +56,94 @@ function theyDidIt(){
         velocity of the video will change, while the main axis velocity will remain constant. On random intervals between 6 and 10 seconds
         the main axis velocity, aka direction, will change.
     */
-    let smallChangeCounter = randBetween(800,1000);
-    let bigChangeCounter = randBetween(3000,6000);
-    
+    let smallChangeCounter = randBetween(800, 1000);
+    let bigChangeCounter = randBetween(3000, 6000);
+
     let smallStart = 0;
     let bigStart = 0;
     let xvel, yvel = 0;
-    let direction = randBetween(0,3);
+    let direction = randBetween(0, 3);
     let xvelpos = 1;
-    let yvelpos = 1; 
-    let offset = (Math.random()+Math.random())/10;
+    let yvelpos = 1;
+    let offset = (Math.random() + Math.random()) / 10;
     let degree = 0;
     let done = 0;
+
     function step(timestamp) {
         let smallProgress = timestamp - smallStart;
         let bigProgress = timestamp - bigStart;
 
-        if (smallProgress>smallChangeCounter){
+        if (smallProgress > smallChangeCounter) {
             smallStart = timestamp;
             [yvel, xvel, yvelpos, xvelpos] = movement(direction);
-            smallChangeCounter = randBetween(1000,3000);
-            offset = (Math.random()+Math.random())/10 + Math.random()/15;
+            smallChangeCounter = randBetween(1000, 3000);
+            offset = (Math.random() + Math.random()) / 10 + Math.random() / 15;
         }
-        if (bigProgress>bigChangeCounter){
+        if (bigProgress > bigChangeCounter) {
             bigStart = timestamp;
-            direction = randBetween(0,3);
-            bigChangeCounter = randBetween(6000,10000);
+            direction = randBetween(0, 3);
+            bigChangeCounter = randBetween(6000, 10000);
             [yvel, xvel, yvelpos, xvelpos] = movement(direction);
-            
+
         }
-        
-        xvel+=xvelpos*offset;
-        yvel+=yvelpos*offset;
-        xpos+=xvel*1.4;
-        ypos+=yvel*1.4;
-        
-        if (xpos+0.33*screenWidth>screenWidth){
+
+        xvel += xvelpos * offset;
+        yvel += yvelpos * offset;
+        xpos += xvel * 1.4;
+        ypos += yvel * 1.4;
+
+        if (xpos + 0.33 * screenWidth > screenWidth) {
             xpos = 0;
-            if (Math.random()>0.7) bigStart = 0;
+            if (Math.random() > 0.7) bigStart = 0;
         }
-        if (xpos<0){
-            xpos = screenWidth-0.34*screenWidth;
-            if (Math.random()>0.7) bigStart = 0;
+        if (xpos < 0) {
+            xpos = screenWidth - 0.34 * screenWidth;
+            if (Math.random() > 0.7) bigStart = 0;
         }
-        if (ypos+0.1875*screenWidth>screenHeight){
+        if (ypos + 0.1875 * screenWidth > screenHeight) {
             ypos = 0;
-            if (Math.random()>0.7) bigStart = 0;
+            if (Math.random() > 0.7) bigStart = 0;
         }
-        if (ypos<0){
-            ypos = screenHeight-0.1875*screenWidth;
-            if (Math.random()>0.7) bigStart = 0;
+        if (ypos < 0) {
+            ypos = screenHeight - 0.1875 * screenWidth;
+            if (Math.random() > 0.7) bigStart = 0;
         }
-        if (player.getCurrentTime()>48.7 && !done) {degree = 0; done = 1; let boom = new Audio("Sound/boom.mp3"); boom.play();
-        let lastThing = document.getElementById("why");
-        lastThing.style.display = "contents";
-        clearInterval(stopColors);
-        x.style.backgroundColor = "black";
-        }
-        else if (player.getCurrentTime()>42.5) degree +=3
-        else if (player.getCurrentTime()>33.5) degree-=1;
-        else if (player.getCurrentTime()>23.9) degree += 4;
-        else if (player.getCurrentTime()>17.65) degree+=3;
-        else if (player.getCurrentTime()>11.6) degree+=2;
-        else if (player.getCurrentTime()>6.2) degree+=1;
-        
-        if (done){
-            x.style.marginLeft = screenWidth/2-0.17*screenWidth + "px";
-            x.style.marginTop = screenHeight/2-((0.1875*screenWidth)/2) + "px";
+        if (player.getCurrentTime() > 48.7 && !done) {
+            degree = 0;
+            done = 1;
+            let boom = new Audio("Sound/boom.mp3");
+            boom.play();
+            let lastThing = document.getElementById("why");
+            lastThing.style.display = "inline-block";
+            clearInterval(stopColors);
+            x.style.backgroundColor = "black";
+        } else if (player.getCurrentTime() > 42.5) degree += 3
+        else if (player.getCurrentTime() > 33.5) degree -= 1;
+        else if (player.getCurrentTime() > 23.9) degree += 4;
+        else if (player.getCurrentTime() > 17.65) degree -= 3;
+        else if (player.getCurrentTime() > 11.6) degree += 2;
+        else if (player.getCurrentTime() > 6.2) degree += 1;
+
+        if (done) {
+            let exp = document.getElementById("explosion");
+            exp.style.display = "inline-block";
+            x.style.marginLeft = screenWidth / 2 - 0.17 * screenWidth + "px";
+            x.style.marginTop = screenHeight / 2 - ((0.1875 * screenWidth) / 2) + "px";
             x.style.transform = "rotate(" + degree + "deg)";
             return;
         }
-        x.style.transform = "rotate(" + degree*3 + "deg)";
-        x.style.marginLeft=xpos+"px";
-        x.style.marginTop=ypos+"px";
-        
+        x.style.transform = "rotate(" + degree * 3 + "deg)";
+        x.style.marginLeft = xpos + "px";
+        x.style.marginTop = ypos + "px";
+
         window.requestAnimationFrame(step);
-      }
-      window.requestAnimationFrame(step);
+    }
+    window.requestAnimationFrame(step);
     //bad practice, but shouldn't be an issue since this is for fun
-    
+
 }
 
-function onYouTubeIframeAPIReady(){
+function onYouTubeIframeAPIReady() {
     player = new YT.Player('player', {
         videoId: "fAaFsc2VXhk",
         playerVars: {
@@ -150,22 +156,23 @@ function onYouTubeIframeAPIReady(){
         }
     });
     let x = document.getElementById('player');
-    x.style.marginLeft = screenWidth/2-0.17*screenWidth + "px";
-    x.style.marginTop = screenHeight/2-((0.1875*screenWidth)/2) + "px";
+    x.style.marginLeft = screenWidth / 2 - 0.17 * screenWidth + "px";
+    x.style.marginTop = screenHeight / 2 - ((0.1875 * screenWidth) / 2) + "px";
 }
-function leggo(e){
+
+function leggo(e) {
     console.log(e.data);
-    if (e.data === 2){
+    if (e.data === 2) {
         e.target.playVideo();
     }
-    if (e.data === 3){
+    if (e.data === 3) {
         e.target.playVideo();
         e.target.setVolume(100);
     }
-    if (e.data === 1){
+    if (e.data === 1) {
         audio.play();
-        let abs = document.getElementById("abs");
-        abs.style.display="none";
+        Prep();
+
         itHasBegun();
         //used the commented out bit to find the perfect times to adjust rotation
         //player.setPlaybackRate(0.25);
@@ -174,56 +181,71 @@ function leggo(e){
         //}, 1);
     }
 }
-function why(){
-    let why = new Audio("Sound/why.mp3");
-    why.play();
+
+function show(id, timeAppear, endAt) {
+    let elem = document.getElementById(id);
+    setTimeout(
+        () => elem.style.display = "inline-block",
+        timeAppear
+    );
+    setTimeout(
+        () => elem.style.display = "none",
+        endAt
+    );
 }
 
-function blaster1(){
-    let blast = new Audio("Sound/blaster.mp3");
-    blast.play();
+function Prep() {
+    function unDesplayAbs() {
+        let abs = document.getElementById("abs");
+        abs.style.display = "none";
+    }
+
+    function positionExplosion() {
+        let x = document.getElementById("explosion");
+        x.style.marginLeft = screenWidth / 2 - 0.17 * screenWidth + "px";
+        x.style.marginTop = screenHeight / 2 - ((0.1875 * screenWidth) / 2) + "px";
+    }
+
+    function enableMedia() {
+        let media = document.getElementById("media");
+        media.style.display = "block";
+    }
+
+    unDesplayAbs();
+    positionExplosion();
+    enableMedia();
 }
 
-function blaster2(){
-    let blast = new Audio("Sound/blaster2.mp3");
-    blast.play();
-}
+function itHasBegun() {
+    function play(file) {
+        let why = new Audio(file);
+        why.play();
+    }
 
-function stop(){
-    let stop = new Audio("Sound/stop.mp3");
-    stop.play();
-}
+    setTimeout(theyDidIt, 3700);
+    for (let i = 1; i < 5; i++) {
+        show("frog" + i, 9000, 13000);
+    }
+    show("commLab", 13000, 19000);
+    show("mariet", 15000, 19000);
+    show("studentPortal", 17000, 19000);
+    show("who", 24000, 32000);
+    show("stop", 26000, 32000);
+    show("wPost", 28000, 32000);
+    show("noHello", 30000, 32000);
+    setTimeout(() => play("Sound/why.mp3"), 9000);
+    setTimeout(() => play("Sound/blaster.mp3"), 13000);
+    setTimeout(() => play("Sound/blaster2.mp3"), 17000);
+    setTimeout(() => play("Sound/okay.mp3"), 21000);
+    setTimeout(() => play("Sound/hThere.mp3"), 25000);
+    setTimeout(() => play("Sound/blaster.mp3"), 27000);
+    setTimeout(() => play("Sound/yup.mp3"), 30000);
+    setTimeout(() => play("Sound/Adele.mp3"), 34000);
+    setTimeout(() => play("Sound/stop.mp3"), 39000);
 
-function dude(){
-    let dude = new Audio("Sound/hThere.mp3");
-    dude.play();
-}
-
-function adele(){
-    let adele = new Audio("Sound/Adele.mp3");
-    adele.play();
-}
-function okay(){
-    let okay = new Audio("Sound/okay.mp3");
-    okay.play();
-}
-function yup(){
-    let okay = new Audio("Sound/yup.mp3");
-    okay.play();
-}
-function itHasBegun(){
-    setTimeout(theyDidIt,3700);
-    setTimeout(why,9000);
-    setTimeout(blaster1,13000);
-    setTimeout(blaster2,17000);
-    setTimeout(okay,21000);
-    setTimeout(dude, 25000);
-    setTimeout(blaster1,27000);
-    setTimeout(yup, 30000);
-    setTimeout(adele, 34000);
-    setTimeout(stop, 39000);
-
-    setTimeout(function(){stopColors=setInterval(function(){
-        document.getElementById('whatHaveIDone').style.backgroundColor = "rgb(" + randBetween(0,255) + "," + randBetween(0,255) + "," + randBetween(0,255) + ")";
-            }, 250)}, 3700);
+    setTimeout(function() {
+        stopColors = setInterval(function() {
+            document.getElementById('whatHaveIDone').style.backgroundColor = "rgb(" + randBetween(0, 255) + "," + randBetween(0, 255) + "," + randBetween(0, 255) + ")";
+        }, 250)
+    }, 3700);
 }
